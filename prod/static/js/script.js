9867868a9547f6368464fc886feea63043a25b65
yaskabkri <yaskabkri@gmail.com>
2024-02-07 02:01:18 +0300

    lieas

--- prod/static/js/script.js
+++ prod/static/js/script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("menu-button").addEventListener("click", function() {
     document.getElementById("sidebar").classList.toggle("sidebar-visible");
 });
--- users/models.py
+++ users/models.py
@@ -2,8 +2,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from django.contrib.auth.models import User
-from django.db import models
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
